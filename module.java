8bef993661238567f280e60acbc33b6a70a88741
Karthick-1999 <karthicksubramaniyan1999.gmail.com>
2025-05-22 12:11:36 +0530

    i added the module

---
+++ module.java
@@ -0,0 +1,12 @@
+import java.util.Scanne;
+
+public class module {
+
+    public static void manin(String [args]){
+
+        Scannner input =new Scanner (system.in);
+        System.out.println ("enter your user name ");
+        String uName =input.nextLine();
+
+        system.out.println(uName);    }
+}
