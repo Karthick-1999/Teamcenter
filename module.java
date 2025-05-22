f4c2a737ef638c9e5a049174be1f665e6998a02a
Karthick-1999 <karthicksubramaniyan1999.gmail.com>
2025-05-22 12:22:53 +0530

    pushing into the main branch

---
+++ module.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class module {
+
+    public static void manin(String [args]){
+
+        Scannner input =new Scanner (system.in);
+        System.out.println ("enter your user name ");
+        String uName =input.nextLine();
+        System.out.println ("enter your password");
+        String password = input.nextLine();
+        system.out.println(uName);
+        System.out.println(password);  
+        
+         }
+}
